accc650c99a0b1221504d74f07f91af67eaa90e0
BrotherTech-BH <worleymartinsns@gmail.com>
2021-09-07 11:37:46 -0300

    up

---
+++ aula.js
@@ -0,0 +1,167 @@
+          //VARIÁVEIS
+
+//let idade = 5;
+//console,console.log(idade);
+// let altura = 175;
+// let nome = 'Worley';
+// console.log(altura, nome);
+// let valorIngressoAdulto = 20;
+// valorIngressoAdulto = 40;
+// console.log(valorIngressoAdulto)
+
+
+    //TIPOS PRIMITIVOS
+//REFERÊNCIA
+
+// let nome = 'Worley'; //string literal
+// let idade = 37; //number literal
+// let estaAprovado = true; //boolean
+// let sobrenome = undefined; //Undefined
+// let corSelecionada = null; //Redefinir um valor
+
+
+  
+
+
+// let nome = 'Worley';
+// let idade = 37;
+// let estaAprovado = true;
+// let sobrenome = undefined;
+
+// let pessoa = {
+//     nome: 'Worley',
+//     idade: 37,
+//     estaAprovado: true,
+//     sobrenome: 'Sacramento'
+// };
+// console.log(pessoa);
+
+
+
+    //ARRAYS
+// let familia = [true, 45, 'Worley', 37];
+// console.log(familia.length)
+// console.log(familia[1]);
+
+
+    //FUNCTIONS
+ //Verbo + Substantivo
+//  let corSite ='azul';
+//  function resetaCor(cor,tonalidade){
+//     corSite = cor + ' ' + tonalidade;
+// };  
+ 
+//  console.log(corSite);
+//  resetaCor('vermelho','pomba-gira')
+//  console.log(corSite);
+
+
+
+
+          //TIPOS DE FUNÇÕES
+//realizar uma tarefa, não devolve nada
+
+// function dizerNome(){
+//     console.log('Worley');
+// }
+// dizerNome();
+
+// //faz um cálculo ou operação e retorna algo
+
+// function multiplicarPorDois(valor){
+//     return valor* 2;
+// }
+//     //console.log(multiplicarPorDois(5));
+
+
+// let resultado = multiplicarPorDois(20);
+// console.log(resultado);
+
+
+          //OPERADORES
+//Operadores Aritiméticos(matemáticos) + , - , * , / , ** exponencial
+// let salario = 100;
+
+// console.log(salario + salario);
+// console.log(salario - salario);
+// console.log(salario * salario);
+// console.log(salario / salario);
+// console.log(5 ** 5);
+
+// ++incremento --decremento
+// let idade = 18;
+
+// console.log(++idade)
+
+
+
+//Operadores de Atribuição
+// let valorTeclado = 100;
+// //valorTeclado = valorTeclado + valorTeclado;
+// //valorTeclado = valorTeclado - valorTeclado;
+// // valorTeclado += valorTeclado;
+// // valorTeclado -= valorTeclado;
+// // console.log(valorTeclado)
+
+
+
+
+    //Operadores de Comparação
+//igualdade estrita compara os tipos (Forama Recomendada)
+// console.log(1 === 1);
+// console.log('1' === 1);
+
+//igualdade solta nao compara os tips ignora a string e compara valor
+// console.log(1 == 1);
+// console.log('1' == 1);
+
+
+
+
+
+    //Operadores ternários
+//tem um cliente, 100 premium, comum
+// let pontos = 200;
+// let tipo = pontos > 100 ? 'premium' : 'comum';
+// console.log(tipo);
+
+
+
+
+   //Operadores Lógicos
+
+//Operador logico E (&&)
+//Retorna TRUE se os dois operando forem true
+// console.log(true && true);
+// console.log(true && false);
+// let maiordeIdade = true;
+// let possuiarteiradeTrabalho = true;
+// let podeAplicar = maiordeIdade && possuiarteiradeTrabalho;
+// console.log(podeAplicar);
+
+//Operador lógico OU (||)
+//Retorna TRUE se um dos operando forem true
+// let maiordeIdade = false;
+// let possuiarteiradeTrabalho = false;
+// let podeAplicar = maiordeIdade || possuiarteiradeTrabalho;
+// console.log(podeAplicar);
+
+//Operador Lógico NOT (!)
+//
+// let maiordeIdade = true;
+// let possuiarteiradeTrabalho = false;
+// let podeAplicar = maiordeIdade || possuiarteiradeTrabalho;
+//  console.log('pode aplicsar: ', podeAplicar);
+
+
+// let candidatoRecusado = !podeAplicar
+
+// console.log('candidato recusado: ' ,candidatoRecusado);
+
+
+
+
+
+
+
+//Operadores Bitwise
---
+++ scripts.js
@@ -0,0 +1,13 @@
+let nome = 'Worley';
+let idade = 37;
+let estaAprovado = true;
+let sobrenome = undefined;
+
+let pessoa = {
+    nome: 'Worley',
+    idade: 37,
+    estaAprovado: true,
+    sobrenome: 'Sacramento'
+};
+
+console.log(pessoa);
